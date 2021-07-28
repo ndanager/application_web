a86549ff2b907ba04bf9f9d66429fb5a9227100c
David Grudl <david@grudl.com>
2021-07-28 12:44:47 +0200

    used native PHP 8 features

--- src/Application/UI/Presenter.php
+++ src/Application/UI/Presenter.php
@@ -142,7 +142,7 @@ abstract class Presenter extends Control implements Application\IPresenter
 	public function isModuleCurrent(string $module): bool
 	{
 		$current = Helpers::splitName($this->getName())[0];
-		return Nette\Utils\Strings::startsWith($current . ':', ltrim($module . ':', ':'));
+		return str_starts_with($current . ':', ltrim($module . ':', ':'));
 	}
 
 
@@ -791,7 +791,7 @@ abstract class Presenter extends Control implements Application\IPresenter
 				if (array_key_exists(0, $args)) {
 					throw new InvalidLinkException("Unable to pass parameters to 'this!' signal.");
 				}
-			} elseif (strpos($signal, self::NAME_SEPARATOR) === false) {
+			} elseif (!str_contains($signal, self::NAME_SEPARATOR)) {
 				// counterpart of signalReceived() & tryCall()
 				$method = $component->formatSignalMethod($signal);
 				if (!$reflection->hasCallableMethod($method)) {
@@ -1027,7 +1027,7 @@ abstract class Presenter extends Control implements Application\IPresenter
 					$name,
 					$rm->getDeclaringClass()->getName() . '::' . $rm->getName(),
 					$type,
-					is_object($args[$name]) ? get_class($args[$name]) : gettype($args[$name]),
+					get_debug_type($args[$name]),
 				));
 			}
 
