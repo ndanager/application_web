nette/application

added property typehints

src/Application/Application.php
@@ -21,50 +21,37 @@ class Application
 {
 	use Nette\SmartObject;
 
-	/** @var int */
-	public $maxLoop = 20;
+	public int $maxLoop = 20;
 
-	/** @var bool enable fault barrier? */
-	public $catchExceptions;
-
-	/** @var string|null */
-	public $errorPresenter;
+	/** enable fault barrier? */
+	public bool $catchExceptions = false;
+	public ?string $errorPresenter = null;
 
 	/** @var array<callable(self): void>  Occurs before the application loads presenter */
-	public $onStartup = [];
+	public iterable $onStartup = [];
 
 	/** @var array<callable(self, ?\Throwable): void>  Occurs before the application shuts down */
-	public $onShutdown = [];
+	public iterable $onShutdown = [];
 
 	/** @var array<callable(self, Request): void>  Occurs when a new request is received */
-	public $onRequest = [];
+	public iterable $onRequest = [];
 
 	/** @var array<callable(self, IPresenter): void>  Occurs when a presenter is created */
-	public $onPresenter = [];
+	public iterable $onPresenter = [];
 
 	/** @var array<callable(self, Response): void>  Occurs when a new response is ready for dispatch */
-	public $onResponse = [];
+	public iterable $onResponse = [];
 
 	/** @var array<callable(self, \Throwable): void>  Occurs when an unhandled exception occurs in the application */
-	public $onError = [];
+	public iterable $onError = [];
 
 	/** @var Request[] */
-	private $requests = [];
-
-	/** @var IPresenter|null */
-	private $presenter;
-
-	/** @var Nette\Http\IRequest */
-	private $httpRequest;
-
-	/** @var Nette\Http\IResponse */
-	private $httpResponse;
-
-	/** @var IPresenterFactory */
-	private $presenterFactory;
-
-	/** @var Router */
-	private $router;
+	private array $requests = [];
+	private ?IPresenter $presenter = null;
+	private Nette\Http\IRequest $httpRequest;
+	private Nette\Http\IResponse $httpResponse;
+	private IPresenterFactory $presenterFactory;
+	private Router $router;
 
 
 	public function __construct(

src/Application/UI/Component.php
@@ -27,10 +27,9 @@ abstract class Component extends Nette\ComponentModel\Container implements Signa
 	use Nette\ComponentModel\ArrayAccess;
 
 	/** @var array<callable(self): void>  Occurs when component is attached to presenter */
-	public $onAnchor = [];
+	public iterable $onAnchor = [];
 
-	/** @var array */
-	protected $params = [];
+	protected array $params = [];
 
 
 	/**

src/Application/UI/Form.php
@@ -18,7 +18,7 @@ use Nette;
 class Form extends Nette\Forms\Form implements SignalReceiver
 {
 	/** @var array<callable(self): void>  Occurs when form is attached to presenter */
-	public $onAnchor = [];
+	public iterable $onAnchor = [];
 
 	/** @var bool */
 	protected $crossOrigin = false;

src/Application/UI/Presenter.php
@@ -45,92 +45,46 @@ abstract class Presenter extends Control implements Application\IPresenter
 		FLASH_KEY = '_fid',
 		DEFAULT_ACTION = 'default';
 
-	/** @var int */
-	public $invalidLinkMode;
+	public int $invalidLinkMode = 0;
 
 	/** @var array<callable(self): void>  Occurs when the presenter is starting */
-	public $onStartup = [];
+	public iterable $onStartup = [];
 
 	/** @var array<callable(self): void>  Occurs when the presenter is rendering after beforeRender */
-	public $onRender = [];
+	public iterable $onRender = [];
 
 	/** @var array<callable(self, Application\Response): void>  Occurs when the presenter is shutting down */
-	public $onShutdown = [];
+	public iterable $onShutdown = [];
 
-	/** @var bool  automatically call canonicalize() */
-	public $autoCanonicalize = true;
+	/** automatically call canonicalize() */
+	public bool $autoCanonicalize = true;
 
-	/** @var bool  use absolute Urls or paths? */
-	public $absoluteUrls = false;
+	/** use absolute Urls or paths? */
+	public bool $absoluteUrls = false;
 
-	/** @var Nette\Application\Request|null */
-	private $request;
-
-	/** @var Nette\Application\Response */
-	private $response;
-
-	/** @var array */
-	private $globalParams = [];
-
-	/** @var array */
-	private $globalState;
-
-	/** @var array|null */
-	private $globalStateSinces;
-
-	/** @var string */
-	private $action;
-
-	/** @var string */
-	private $view;
-
-	/** @var string|bool */
-	private $layout;
-
-	/** @var \stdClass */
-	private $payload;
-
-	/** @var string */
-	private $signalReceiver;
-
-	/** @var string|null */
-	private $signal;
-
-	/** @var bool */
-	private $ajaxMode;
-
-	/** @var bool */
-	private $startupCheck;
-
-	/** @var Nette\Application\Request|null */
-	private $lastCreatedRequest;
-
-	/** @var array|null */
-	private $lastCreatedRequestFlag;
-
-	/** @var Nette\Http\IRequest */
-	private $httpRequest;
-
-	/** @var Nette\Http\IResponse */
-	private $httpResponse;
-
-	/** @var Nette\Http\Session */
-	private $session;
-
-	/** @var Nette\Application\IPresenterFactory */
-	private $presenterFactory;
-
-	/** @var Nette\Routing\Router */
-	private $router;
-
-	/** @var Nette\Security\User */
-	private $user;
-
-	/** @var TemplateFactory */
-	private $templateFactory;
-
-	/** @var Nette\Http\UrlScript */
-	private $refUrlCache;
+	private ?Nette\Application\Request $request = null;
+	private ?Nette\Application\Response $response = null;
+	private array $globalParams = [];
+	private array $globalState;
+	private ?array $globalStateSinces;
+	private string $action = '';
+	private string $view = '';
+	private string|bool $layout = '';
+	private \stdClass $payload;
+	private string $signalReceiver;
+	private ?string $signal = null;
+	private bool $ajaxMode;
+	private bool $startupCheck = false;
+	private ?Nette\Application\Request $lastCreatedRequest;
+	private ?array $lastCreatedRequestFlag;
+	private Nette\Http\IRequest $httpRequest;
+	private Nette\Http\IResponse $httpResponse;
+	private ?Nette\Http\Session $session = null;
+	private ?Nette\Application\IPresenterFactory $presenterFactory = null;
+	private ?Nette\Routing\Router $router = null;
+	private ?Nette\Security\User $user = null;
+	private ?TemplateFactory $templateFactory = null;
+	private Nette\Http\UrlScript $refUrlCache;
 
 
 	public function __construct()
@@ -188,7 +142,7 @@ abstract class Presenter extends Control implements Application\IPresenter
 	public function run(Application\Request $request): Application\Response
 	{
 		$this->request = $request;
-		$this->payload = $this->payload ?: new \stdClass;
+		$this->payload ??= new \stdClass;
 		$this->setParent($this->getParent(), $request->getPresenterName());
 
 		if (!$this->httpResponse->isSent()) {
@@ -322,7 +276,7 @@ abstract class Presenter extends Control implements Application\IPresenter
 	 */
 	public function processSignal(): void
 	{
-		if ($this->signal === null) {
+		if (!isset($this->signal)) {
 			return;
 		}
 
@@ -587,7 +541,7 @@ abstract class Presenter extends Control implements Application\IPresenter
 	 */
 	public function isAjax(): bool
 	{
-		if ($this->ajaxMode === null) {
+		if (!isset($this->ajaxMode)) {
 			$this->ajaxMode = $this->httpRequest->isAjax();
 		}
 
@@ -989,7 +943,7 @@ abstract class Presenter extends Control implements Application\IPresenter
 	 */
 	protected function requestToUrl(Application\Request $request, ?bool $relative = null): string
 	{
-		if ($this->refUrlCache === null) {
+		if (!isset($this->refUrlCache)) {
 			$url = $this->httpRequest->getUrl();
 			$this->refUrlCache = new Http\UrlScript($url->withoutUserInfo()->getHostUrl() . $url->getScriptPath());
 		}
@@ -1173,7 +1127,7 @@ abstract class Presenter extends Control implements Application\IPresenter
 	{
 		$sinces = &$this->globalStateSinces;
 
-		if ($this->globalState === null) {
+		if (!isset($this->globalState)) {
 			$state = [];
 			foreach ($this->globalParams as $id => $params) {
 				$prefix = $id . self::NAME_SEPARATOR;
@@ -1386,7 +1340,7 @@ abstract class Presenter extends Control implements Application\IPresenter
 		?Nette\Security\User $user = null,
 		?TemplateFactory $templateFactory = null,
 	) {
-		if ($this->presenterFactory !== null) {
+		if (isset($this->presenterFactory)) {
 			throw new Nette\InvalidStateException('Method ' . __METHOD__ . ' is intended for initialization and should not be called more than once.');
 		}
 

src/Bridges/ApplicationLatte/TemplateFactory.php
@@ -22,22 +22,12 @@ class TemplateFactory implements UI\TemplateFactory
 	use Nette\SmartObject;
 
 	/** @var array<callable(Template): void>  Occurs when a new template is created */
-	public $onCreate = [];
-
-	/** @var LatteFactory */
-	private $latteFactory;
-
-	/** @var Nette\Http\IRequest|null */
-	private $httpRequest;
-
-	/** @var Nette\Security\User|null */
-	private $user;
-
-	/** @var Nette\Caching\Storage|null */
-	private $cacheStorage;
-
-	/** @var string */
-	private $templateClass;
+	public iterable $onCreate = [];
+	private LatteFactory $latteFactory;
+	private ?Nette\Http\IRequest $httpRequest;
+	private ?Nette\Security\User $user;
+	private ?Nette\Caching\Storage $cacheStorage;
+	private string $templateClass;
 
 
 	public function __construct(

ndanager/application_web

src/Application/Application.php
@@ -21,50 +21,37 @@ class Application
 {
 	use Nette\SmartObject;
 
-	/** @var int */
-	public $maxLoop = 20;
+	public int $maxLoop = 20;
 
-	/** @var bool enable fault barrier? */
-	public $catchExceptions;
-
-	/** @var string|null */
-	public $errorPresenter;
+	/** enable fault barrier? */
+	public bool $catchExceptions = false;
+	public ?string $errorPresenter = null;
 
 	/** @var array<callable(self): void>  Occurs before the application loads presenter */
-	public $onStartup = [];
+	public array $onStartup = [];
 
 	/** @var array<callable(self, ?\Throwable): void>  Occurs before the application shuts down */
-	public $onShutdown = [];
+	public array $onShutdown = [];
 
 	/** @var array<callable(self, Request): void>  Occurs when a new request is received */
-	public $onRequest = [];
+	public array $onRequest = [];
 
 	/** @var array<callable(self, IPresenter): void>  Occurs when a presenter is created */
-	public $onPresenter = [];
+	public array $onPresenter = [];
 
 	/** @var array<callable(self, Response): void>  Occurs when a new response is ready for dispatch */
-	public $onResponse = [];
+	public array $onResponse = [];
 
 	/** @var array<callable(self, \Throwable): void>  Occurs when an unhandled exception occurs in the application */
-	public $onError = [];
+	public array $onError = [];
 
 	/** @var Request[] */
-	private $requests = [];
-
-	/** @var IPresenter|null */
-	private $presenter;
-
-	/** @var Nette\Http\IRequest */
-	private $httpRequest;
-
-	/** @var Nette\Http\IResponse */
-	private $httpResponse;
-
-	/** @var IPresenterFactory */
-	private $presenterFactory;
-
-	/** @var Router */
-	private $router;
+	private array $requests = [];
+	private ?IPresenter $presenter = null;
+	private Nette\Http\IRequest $httpRequest;
+	private Nette\Http\IResponse $httpResponse;
+	private IPresenterFactory $presenterFactory;
+	private Router $router;
 
 
 	public function __construct(

src/Application/UI/Component.php
@@ -27,10 +27,9 @@ abstract class Component extends Nette\ComponentModel\Container implements Signa
 	use Nette\ComponentModel\ArrayAccess;
 
 	/** @var array<callable(self): void>  Occurs when component is attached to presenter */
-	public $onAnchor = [];
+	public array $onAnchor = [];
 
-	/** @var array */
-	protected $params = [];
+	protected array $params = [];
 
 
 	/**

src/Application/UI/Form.php
@@ -18,7 +18,7 @@ use Nette;
 class Form extends Nette\Forms\Form implements SignalReceiver
 {
 	/** @var array<callable(self): void>  Occurs when form is attached to presenter */
-	public $onAnchor = [];
+	public array $onAnchor = [];
 
 	/** @var bool */
 	protected $crossOrigin = false;

src/Application/UI/Presenter.php
@@ -55,92 +55,46 @@ abstract class Presenter extends Control implements Application\IPresenter
 	public const FLASH_KEY = self::FlashKey;
 	public const DEFAULT_ACTION = self::DefaultAction;
 
-	/** @var int */
-	public $invalidLinkMode;
+	public int $invalidLinkMode = 0;
 
 	/** @var array<callable(self): void>  Occurs when the presenter is starting */
-	public $onStartup = [];
+	public array $onStartup = [];
 
 	/** @var array<callable(self): void>  Occurs when the presenter is rendering after beforeRender */
-	public $onRender = [];
+	public array $onRender = [];
 
 	/** @var array<callable(self, Application\Response): void>  Occurs when the presenter is shutting down */
-	public $onShutdown = [];
+	public array $onShutdown = [];
 
-	/** @var bool  automatically call canonicalize() */
-	public $autoCanonicalize = true;
+	/** automatically call canonicalize() */
+	public bool $autoCanonicalize = true;
 
-	/** @var bool  use absolute Urls or paths? */
-	public $absoluteUrls = false;
+	/** use absolute Urls or paths? */
+	public bool $absoluteUrls = false;
 
-	/** @var Nette\Application\Request|null */
-	private $request;
-
-	/** @var Nette\Application\Response */
-	private $response;
-
-	/** @var array */
-	private $globalParams = [];
-
-	/** @var array */
-	private $globalState;
-
-	/** @var array|null */
-	private $globalStateSinces;
-
-	/** @var string */
-	private $action;
-
-	/** @var string */
-	private $view;
-
-	/** @var string|bool */
-	private $layout;
-
-	/** @var \stdClass */
-	private $payload;
-
-	/** @var string */
-	private $signalReceiver;
-
-	/** @var string|null */
-	private $signal;
-
-	/** @var bool */
-	private $ajaxMode;
-
-	/** @var bool */
-	private $startupCheck;
-
-	/** @var Nette\Application\Request|null */
-	private $lastCreatedRequest;
-
-	/** @var array|null */
-	private $lastCreatedRequestFlag;
-
-	/** @var Nette\Http\IRequest */
-	private $httpRequest;
-
-	/** @var Nette\Http\IResponse */
-	private $httpResponse;
-
-	/** @var Nette\Http\Session */
-	private $session;
-
-	/** @var Nette\Application\IPresenterFactory */
-	private $presenterFactory;
-
-	/** @var Nette\Routing\Router */
-	private $router;
-
-	/** @var Nette\Security\User */
-	private $user;
-
-	/** @var TemplateFactory */
-	private $templateFactory;
-
-	/** @var Nette\Http\UrlScript */
-	private $refUrlCache;
+	private ?Nette\Application\Request $request = null;
+	private ?Nette\Application\Response $response = null;
+	private array $globalParams = [];
+	private array $globalState;
+	private ?array $globalStateSinces;
+	private string $action = '';
+	private string $view = '';
+	private string|bool $layout = '';
+	private \stdClass $payload;
+	private string $signalReceiver;
+	private ?string $signal = null;
+	private bool $ajaxMode;
+	private bool $startupCheck = false;
+	private ?Nette\Application\Request $lastCreatedRequest;
+	private ?array $lastCreatedRequestFlag;
+	private Nette\Http\IRequest $httpRequest;
+	private Nette\Http\IResponse $httpResponse;
+	private ?Nette\Http\Session $session = null;
+	private ?Nette\Application\IPresenterFactory $presenterFactory = null;
+	private ?Nette\Routing\Router $router = null;
+	private ?Nette\Security\User $user = null;
+	private ?TemplateFactory $templateFactory = null;
+	private Nette\Http\UrlScript $refUrlCache;
 
 
 	public function __construct()
@@ -198,7 +152,7 @@ abstract class Presenter extends Control implements Application\IPresenter
 	public function run(Application\Request $request): Application\Response
 	{
 		$this->request = $request;
-		$this->payload = $this->payload ?: new \stdClass;
+		$this->payload ??= new \stdClass;
 		$this->setParent($this->getParent(), $request->getPresenterName());
 
 		if (!$this->httpResponse->isSent()) {
@@ -336,7 +290,7 @@ abstract class Presenter extends Control implements Application\IPresenter
 	 */
 	public function processSignal(): void
 	{
-		if ($this->signal === null) {
+		if (!isset($this->signal)) {
 			return;
 		}
 
@@ -601,7 +555,7 @@ abstract class Presenter extends Control implements Application\IPresenter
 	 */
 	public function isAjax(): bool
 	{
-		if ($this->ajaxMode === null) {
+		if (!isset($this->ajaxMode)) {
 			$this->ajaxMode = $this->httpRequest->isAjax();
 		}
 
@@ -1004,7 +958,7 @@ abstract class Presenter extends Control implements Application\IPresenter
 	 */
 	protected function requestToUrl(Application\Request $request, ?bool $relative = null): string
 	{
-		if ($this->refUrlCache === null) {
+		if (!isset($this->refUrlCache)) {
 			$url = $this->httpRequest->getUrl();
 			$this->refUrlCache = new Http\UrlScript($url->withoutUserInfo()->getHostUrl() . $url->getScriptPath());
 		}
@@ -1189,7 +1143,7 @@ abstract class Presenter extends Control implements Application\IPresenter
 	{
 		$sinces = &$this->globalStateSinces;
 
-		if ($this->globalState === null) {
+		if (!isset($this->globalState)) {
 			$state = [];
 			foreach ($this->globalParams as $id => $params) {
 				$prefix = $id . self::NAME_SEPARATOR;
@@ -1402,7 +1356,7 @@ abstract class Presenter extends Control implements Application\IPresenter
 		?Nette\Security\User $user = null,
 		?TemplateFactory $templateFactory = null,
 	) {
-		if ($this->presenterFactory !== null) {
+		if (isset($this->presenterFactory)) {
 			throw new Nette\InvalidStateException('Method ' . __METHOD__ . ' is intended for initialization and should not be called more than once.');
 		}
 

src/Bridges/ApplicationLatte/TemplateFactory.php
@@ -22,22 +22,12 @@ class TemplateFactory implements UI\TemplateFactory
 	use Nette\SmartObject;
 
 	/** @var array<callable(Template): void>  Occurs when a new template is created */
-	public $onCreate = [];
-
-	/** @var LatteFactory */
-	private $latteFactory;
-
-	/** @var Nette\Http\IRequest|null */
-	private $httpRequest;
-
-	/** @var Nette\Security\User|null */
-	private $user;
-
-	/** @var Nette\Caching\Storage|null */
-	private $cacheStorage;
-
-	/** @var string */
-	private $templateClass;
+	public array $onCreate = [];
+	private LatteFactory $latteFactory;
+	private ?Nette\Http\IRequest $httpRequest;
+	private ?Nette\Security\User $user;
+	private ?Nette\Caching\Storage $cacheStorage;
+	private string $templateClass;
 
 
 	public function __construct(